e482f9a83f2f01afeee4eaf5f8d4c1468cfa5d77
rkkhaerul <rkkhaerul@gmail.com>
2020-01-28 14:41:00 +0700

    14:40, 28/01/2020

--- App.js
+++ App.js
@@ -7,15 +7,35 @@ import { MenuProvider } from "react-native-popup-menu";
 import Header from './components/header';
 import HeaderSearchWithButton from './components/headerSearchWithButton';
 import CardUser from './components/cardUserAuthority';
+import CardClient from './components/cardClient'
+import CardProduct from './components/cardProduct';
+import CardPrinciple from './components/cardPrinciple';
+import CardPipeline from './components/cardPipeline'
+import CardSalesAgenda from './components/cardSalesAgenda'
+import CardRoles from './components/cardRolesAuthority'
+import CardFolder from './components/cardFolderFileManagement'
+import CardFile from './components/cardFileFileManagement'
+import CardSignActivity from './components/cardSignActivityLogActivity'
+import CardUsersActivity from './components/cardUsersActivityLogActivity'
 
 export default class App extends Component {
 
     render() {
         return(
             // <CardUser/>
-            <MenuProvider>
-                <DrawerNavigator/>
-            </MenuProvider>
+            // <CardClient/>
+            // <CardProduct/>
+            // <CardPrinciple/>
+            // <CardPipeline/>
+            // <CardSalesAgenda/>
+            // <CardRoles/>
+            // <CardFolder/>
+            // <CardFile/>
+            // <CardSignActivity/>
+            <CardUsersActivity/>
+            // <MenuProvider>
+            //     <DrawerNavigator/>
+            // </MenuProvider>
         )
     }
 }
