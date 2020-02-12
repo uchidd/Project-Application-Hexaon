ddba6f504ec5241c13aeedf647849a868250c736
uchidd <mmi090903@gmail.com>
2020-02-12 11:05:35 +0700

    11:05, 12/02/2020

---
+++ modalDetail/modalDetailPrinciple.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { View, StyleSheet } from "react-native";
+
+export default class ModalDetailPrinciple extends Component{
+    render(){
+        return(
+            <View style={styles.container}>
+
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container : {
+        alignSelf : "stretch",
+        backgroundColor : "red"
+    }
+})
