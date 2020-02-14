ee5d02e2b3c46257b03d5da9e7c8b2f33a9ede73
rkkhaerul <rkkhaerul@gmail.com>
2020-02-14 10:45:09 +0700

    10:44, 14/02/2020

---
+++ modal_details/modalDetailPrinciple.js
@@ -0,0 +1,223 @@
+import React, { Component } from "react";
+import { View, StyleSheet, Text, StatusBar, TouchableOpacity } from "react-native";
+
+export default class ModalDetailPrinciple extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar hidden={true} />
+                <View style={styles.mainView}>
+                    <View style={styles.titleView}>
+                        <Text style={styles.textTitle}>Details</Text>
+                    </View>
+                    <View style={styles.contentView}>
+                        <View style={styles.topRow}>
+                            <View style={styles.leftView}>
+                                <Text style={styles.text}>ID</Text>
+                            </View>
+                            <View style={styles.rightView}>
+                                <Text style={styles.text}>{this.props.idModal}</Text>
+                            </View>
+                        </View>
+                        <View style={styles.row}>
+                            <View style={styles.leftView}>
+                                <Text style={styles.text}>Name</Text>
+                            </View>
+                            <View style={styles.rightView}>
+                                <Text style={styles.text}>{this.props.nameModal}</Text>
+                            </View>
+                        </View>
+                        <View style={styles.row}>
+                            <View style={styles.leftView}>
+                                <Text style={styles.text}>Email</Text>
+                            </View>
+                            <View style={styles.rightView}>
+                                <Text style={styles.text}>{this.props.emailModal}</Text>
+                            </View>
+                        </View>
+                        <View style={styles.row}>
+                            <View style={styles.leftView}>
+                                <Text style={styles.text}>Phone</Text>
+                            </View>
+                            <View style={styles.rightView}>
+                                <Text style={styles.text}>{this.props.phoneModal}</Text>
+                            </View>
+                        </View>
+                        <View style={styles.row}>
+                            <View style={styles.leftView}>
+                                <Text style={styles.text}>Address</Text>
+                            </View>
+                            <View style={styles.rightView}>
+                                <Text style={styles.text}>{this.props.addressModal}</Text>
+                            </View>
+                        </View>
+                        <View style={styles.row}>
+                            <View style={styles.leftView}>
+                                <Text style={styles.text}>PIC Name</Text>
+                            </View>
+                            <View style={styles.rightView}>
+                                <Text style={styles.text}>{this.props.pic_nameModal}</Text>
+                            </View>
+                        </View>
+                        <View style={styles.rowBottom}>
+                            <View style={styles.leftView}>
+                                <Text style={styles.text}>PIC Contact</Text>
+                            </View>
+                            <View style={styles.rightView}>
+                                <Text style={styles.text}>{this.props.pic_contactModal}</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                </View>
+                <TouchableOpacity onPress={this.props.hideModalViewData} style={styles.button}>
+                    <Text style={styles.textButton}>Close</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+<<<<<<< HEAD:modal details/modalDetailPrinciple.js
+    container : {
+        backgroundColor : "rgba(211, 211, 211, 0.5)",
+        padding : 3,
+        flexDirection : "column",
+        justifyContent : "center",
+        flex : 1
+    },
+    mainView : {
+        alignSelf : "stretch",
+        backgroundColor : "#FFFFFF",
+        margin : 3,
+        padding : 3,
+        flexDirection : "column",
+        borderRadius : 5,
+=======
+    container: {
+        alignSelf: "stretch",
+        backgroundColor: "rgba(33, 33, 33, 0.7)",
+        padding: 3,
+        flexDirection: "column",
+        justifyContent: "center",
+        flex: 1
+    },
+    mainView: {
+        alignSelf: "stretch",
+        backgroundColor: "#FFFFFF",
+        margin: 3,
+        padding: 3,
+        flexDirection: "column",
+        borderRadius: 5,
+        borderColor: "#FFFFFF",
+        borderWidth: 2
+>>>>>>> 5377556bccb49dc3a525602736e87ec1d4c95f3e:modals/modalDetailPrinciple.js
+    },
+    titleView: {
+        backgroundColor: "transparent",
+        margin: 3,
+        padding: 3,
+        justifyContent: "center",
+        alignItems: "center",
+        height: 40
+    },
+    textTitle: {
+        fontSize: 24,
+        color: "#2A4580",
+    },
+<<<<<<< HEAD:modal details/modalDetailPrinciple.js
+    contentView : {
+        alignSelf : "stretch",
+        backgroundColor : "#D3D3D3",
+        margin : 3,
+        flexDirection : "column",
+        borderRadius : 5,
+        borderColor : "#2A4580",
+        borderWidth : 2,
+=======
+    contentView: {
+        alignSelf: "stretch",
+        backgroundColor: "#D3D3D3",
+        margin: 3,
+        flexDirection: "column",
+        borderRadius: 3,
+        borderColor: "#2A4580",
+        borderWidth: 2,
+>>>>>>> 5377556bccb49dc3a525602736e87ec1d4c95f3e:modals/modalDetailPrinciple.js
+    },
+    topRow: {
+        alignSelf: "stretch",
+        backgroundColor: "transparent",
+        flexDirection: "row",
+        borderBottomWidth: 1,
+        borderBottomColor: "#2A4580",
+    },
+    leftView: {
+        alignSelf: "stretch",
+        backgroundColor: "transparent",
+        borderRightWidth: 1,
+        borderRightColor: "#2A4580",
+        width: 100,
+        padding: 3,
+        justifyContent: "center"
+    },
+    rightView: {
+        alignSelf: "stretch",
+        backgroundColor: "transparent",
+        borderLeftWidth: 1,
+        borderLeftColor: "#2A4580",
+        padding: 3,
+        flex: 1,
+        justifyContent: "center"
+    },
+    text: {
+        fontSize: 16,
+        color: "#2A4580",
+        margin: 3
+    },
+    row: {
+        alignSelf: "stretch",
+        backgroundColor: "transparent",
+        flexDirection: "row",
+        borderBottomWidth: 1,
+        borderBottomColor: "#2A4580",
+        borderTopColor: "#2A4580",
+        borderTopWidth: 1,
+    },
+    rowBottom: {
+        alignSelf: "stretch",
+        backgroundColor: "transparent",
+        flexDirection: "row",
+        borderTopColor: "#2A4580",
+        borderTopWidth: 1,
+    },
+<<<<<<< HEAD:modal details/modalDetailPrinciple.js
+    button : {
+        height : 50,
+        backgroundColor : "#2A4580",
+        margin : 3,
+        padding : 3,
+        flexDirection : "column",
+        borderRadius : 5,
+        justifyContent : "center",
+        alignItems : "center"
+    },
+    textButton : {
+        fontSize : 24,
+        color : "#FFFFFF",
+=======
+    button: {
+        height: 50,
+        backgroundColor: "#FFFFFF",
+        margin: 3,
+        padding: 3,
+        flexDirection: "column",
+        borderRadius: 5,
+        borderColor: "#FFFFFF",
+        borderWidth: 2,
+        justifyContent: "center",
+        alignItems: "center"
+>>>>>>> 5377556bccb49dc3a525602736e87ec1d4c95f3e:modals/modalDetailPrinciple.js
+    }
+}) 
