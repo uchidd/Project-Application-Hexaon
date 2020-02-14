d58be54de0d2279daec93e309fa15e058d68a428
rkkhaerul <rkkhaerul@gmail.com>
2020-02-14 10:36:57 +0700

    10:36, 14/02/2020

--- modals/modalDetailClient.js
+++ modals/modalDetailClient.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { View, StyleSheet, Text, StatusBar, TouchableOpacity } from "react-native";
 
-export default class ModalDetailClient extends Component{
-    render(){
-        return(
+export default class ModalDetailClient extends Component {
+    render() {
+        return (
             <View style={styles.container}>
-                <StatusBar hidden={true}/>
+                <StatusBar hidden={true} />
                 <View style={styles.mainView}>
                     <View style={styles.titleView}>
                         <Text style={styles.textTitle}>Details</Text>
@@ -68,7 +68,7 @@ export default class ModalDetailClient extends Component{
                             </View>
                         </View>
                     </View>
-                    
+
                 </View>
                 <TouchableOpacity onPress={this.props.hideModalViewData} style={styles.button}>
                     <Text style={styles.textTitle}>Close</Text>
@@ -79,101 +79,101 @@ export default class ModalDetailClient extends Component{
 }
 
 const styles = StyleSheet.create({
-    container : {
-        alignSelf : "stretch",
-        backgroundColor : "rgba(33, 33, 33, 0.7)",
-        padding : 3,
-        flexDirection : "column",
-        justifyContent : "center",
-        flex : 1
+    container: {
+        alignSelf: "stretch",
+        backgroundColor: "rgba(33, 33, 33, 0.7)",
+        padding: 3,
+        flexDirection: "column",
+        justifyContent: "center",
+        flex: 1
     },
-    mainView : {
-        alignSelf : "stretch",
-        backgroundColor : "#222629",
-        margin : 3,
-        padding : 3,
-        flexDirection : "column",
-        borderRadius : 3,
-        borderColor : "#86C232",
-        borderWidth : 2
+    mainView: {
+        alignSelf: "stretch",
+        backgroundColor: "#222629",
+        margin: 3,
+        padding: 3,
+        flexDirection: "column",
+        borderRadius: 3,
+        borderColor: "#86C232",
+        borderWidth: 2
     },
-    titleView : {
-        backgroundColor : "transparent",
-        margin : 3,
-        padding : 3,
-        justifyContent : "center", 
-        alignItems : "center",
-        height : 40
+    titleView: {
+        backgroundColor: "transparent",
+        margin: 3,
+        padding: 3,
+        justifyContent: "center",
+        alignItems: "center",
+        height: 40
     },
-    textTitle : {
-        fontSize : 24,
-        color : "#86C232",
+    textTitle: {
+        fontSize: 24,
+        color: "#86C232",
     },
-    contentView : {
-        alignSelf : "stretch",
-        backgroundColor : "#474B4F",
-        margin : 3,
-        flexDirection : "column",
-        borderRadius : 3,
-        borderColor : "#86C232",
-        borderWidth : 2,
+    contentView: {
+        alignSelf: "stretch",
+        backgroundColor: "#474B4F",
+        margin: 3,
+        flexDirection: "column",
+        borderRadius: 3,
+        borderColor: "#86C232",
+        borderWidth: 2,
     },
-    topRow : {
-        alignSelf : "stretch",
-        backgroundColor : "transparent",
-        flexDirection : "row",
-        borderBottomWidth : 1,
-        borderBottomColor : "#86C232",
+    topRow: {
+        alignSelf: "stretch",
+        backgroundColor: "transparent",
+        flexDirection: "row",
+        borderBottomWidth: 1,
+        borderBottomColor: "#86C232",
     },
-    leftView : {
-        alignSelf : "stretch",
-        backgroundColor : "transparent",
-        borderRightWidth : 1,
-        borderRightColor : "#86C232",
-        width : 100,
-        padding : 3,
-        justifyContent : "center"
+    leftView: {
+        alignSelf: "stretch",
+        backgroundColor: "transparent",
+        borderRightWidth: 1,
+        borderRightColor: "#86C232",
+        width: 100,
+        padding: 3,
+        justifyContent: "center"
     },
-    rightView : {
-        alignSelf : "stretch",
-        backgroundColor : "transparent",
-        borderLeftWidth : 1,
-        borderLeftColor : "#86C232",
-        padding : 3,
-        flex : 1,
-        justifyContent : "center"
+    rightView: {
+        alignSelf: "stretch",
+        backgroundColor: "transparent",
+        borderLeftWidth: 1,
+        borderLeftColor: "#86C232",
+        padding: 3,
+        flex: 1,
+        justifyContent: "center"
     },
-    text : {
-        fontSize : 16,
-        color : "#86C232",
-        margin : 3
+    text: {
+        fontSize: 16,
+        color: "#86C232",
+        margin: 3
     },
-    row : {
-        alignSelf : "stretch",
-        backgroundColor : "transparent",
-        flexDirection : "row",
-        borderBottomWidth : 1,
-        borderBottomColor : "#86C232",
-        borderTopColor : "#86C232",
-        borderTopWidth : 1,
+    row: {
+        alignSelf: "stretch",
+        backgroundColor: "transparent",
+        flexDirection: "row",
+        borderBottomWidth: 1,
+        borderBottomColor: "#86C232",
+        borderTopColor: "#86C232",
+        borderTopWidth: 1,
     },
-    rowBottom : {
-        alignSelf : "stretch",
-        backgroundColor : "transparent",
-        flexDirection : "row",
-        borderTopColor : "#86C232",
-        borderTopWidth : 1,
+    rowBottom: {
+        alignSelf: "stretch",
+        backgroundColor: "transparent",
+        flexDirection: "row",
+        borderTopColor: "#86C232",
+        borderTopWidth: 1,
     },
-    button : {
-        height : 50,
-        backgroundColor : "#222629",
-        margin : 3,
-        padding : 3,
-        flexDirection : "column",
-        borderRadius : 3,
-        borderColor : "#86C232",
-        borderWidth : 2,
-        justifyContent : "center",
-        alignItems : "center"
+    button: {
+        height: 50,
+        backgroundColor: "#222629",
+        margin: 3,
+        padding: 3,
+        flexDirection: "column",
+        borderRadius: 3,
+        borderColor: "#86C232",
+        borderWidth: 2,
+        justifyContent: "center",
+        alignItems: "center"
     }
 }) 
